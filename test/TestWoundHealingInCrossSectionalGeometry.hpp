34dd230c02c642fe8eb34ff2d2beda49cf96588e
Axel Almet <axelalmet@gmail.com>
2020-01-31 16:57:20 -0800

    Updating cross-sectional model.

--- test/TestWoundHealingInCrossSectionalGeometry.hpp
+++ test/TestWoundHealingInCrossSectionalGeometry.hpp
@@ -11,16 +11,19 @@
 #include "CheckpointArchiveTypes.hpp" //Needed if we use GetIdentifier() method (which we do)
 #include "HoneycombMeshGenerator.hpp" //Generates mesh
 #include "DifferentiatedCellProliferativeType.hpp" //Stops cells from proliferating
-#include "LinearSpringForceWithVariableRestLength.hpp"
+#include "GeneralisedLinearSpringForce.hpp"
+#include "RepulsionForce.hpp"
 #include "FixedRegionPlaneBoundaryCondition.hpp" // Fixed-position boundary condition
 #include "VoronoiDataWriter.hpp" //Allows us to visualise output in Paraview
 #include "HoneycombMeshGenerator.hpp" //Generates mesh
 #include "NoCellCycleModel.hpp"
 #include "NodeBasedCellPopulation.hpp"
-#include "NodesOnlyMesh.hpp"
+#include "Cylindrical2dNodesOnlyMesh.hpp"
+// #include "NodesOnlyMesh.hpp"
 #include "OffLatticeSimulation.hpp" //Simulates the evolution of the population
 #include "SmartPointers.hpp" //Enables macros to save typing
 #include "StemCellProliferativeType.hpp"
+#include "TransitCellProliferativeType.hpp"
 #include "WildTypeCellMutationState.hpp"
 #include "FakePetscSetup.hpp" //Forbids tests running in parallel
 #include "PetscSetupAndFinalize.hpp"
@@ -39,28 +42,25 @@ public:
     void TestWounding()
     {
 
-        //Set all the spring stiffness variables
-        // double epithelial_epithelial_stiffness = 45.0;
-        // double epithelial_stromal_stiffness = 45.0;
-        // double stromal_stromal_stiffness = 45.0;
-
         //Set the number of cells across and down for the array
-        unsigned cells_across = 1300;
-        unsigned cells_up = 70;
-
-        //Set the basement membrane force parameters
+        unsigned cells_across = 30;
+        unsigned cells_up = 10;
 
         // double epithelial_epithelial_resting_spring_length = 1.0;
 
-        double radius_of_interaction = 1.5;
+        // double radius_of_interaction = 1.5;
         double division_separation = 0.1;
 
         HoneycombMeshGenerator generator(cells_across, cells_up, 0); //Create mesh
         MutableMesh<2, 2>* p_generating_mesh = generator.GetMesh(); //Generate mesh
 
-        // Construct the mesh that we'll actually use.
-		NodesOnlyMesh<2> mesh;
-        mesh.ConstructNodesWithoutMesh(*p_generating_mesh, radius_of_interaction); //Construct mesh
+        // Construct a periodic mesh
+        Cylindrical2dNodesOnlyMesh mesh(1.0*cells_across);
+		mesh.ConstructNodesWithoutMesh(*p_generating_mesh, 1.0*cells_across); //Construct mesh
+
+        // // Construct the mesh that we'll actually use.
+		// NodesOnlyMesh<2> mesh;
+        // mesh.ConstructNodesWithoutMesh(*p_generating_mesh, radius_of_interaction); //Construct mesh
 
         //Get the real indices
         std::vector<unsigned> location_indices = generator.GetCellLocationIndices();
@@ -69,6 +69,7 @@ public:
         //Create shared pointers for cell and mutation states
         boost::shared_ptr<AbstractCellProperty> p_diff_type = CellPropertyRegistry::Instance()->Get<DifferentiatedCellProliferativeType>();
         boost::shared_ptr<AbstractCellProperty> p_stem_type = CellPropertyRegistry::Instance()->Get<StemCellProliferativeType>();
+        boost::shared_ptr<AbstractCellProperty> p_transit_type = CellPropertyRegistry::Instance()->Get<TransitCellProliferativeType>();
         boost::shared_ptr<AbstractCellProperty> p_wildtype_state = CellPropertyRegistry::Instance()->Get<WildTypeCellMutationState>();
 		boost::shared_ptr<AbstractCellProperty> p_label = CellPropertyRegistry::Instance()->Get<CellLabel>();
 
@@ -84,7 +85,7 @@ public:
             CellPtr p_cell(new Cell(p_wildtype_state, p_cycle_model));
             p_cell->InitialiseCellCycleModel(); // For paranoia really.
 
-            p_cell->SetCellProliferativeType(p_diff_type); //Make cell differentiated
+            p_cell->SetCellProliferativeType(p_transit_type); //Make cell differentiated
 
             cells.push_back(p_cell);
         }
@@ -126,24 +127,36 @@ public:
             }
         }
 
+        // Let's set the cell populations for the boundary cells and the epithelium
         for (AbstractCellPopulation<2>::Iterator cell_iter = cell_population.Begin();
         cell_iter != cell_population.End(); ++cell_iter)
         {
-            double x = cell_population.GetLocationOfCellCentre(*cell_iter)[0];
+            // double x = cell_population.GetLocationOfCellCentre(*cell_iter)[0];
             double y = cell_population.GetLocationOfCellCentre(*cell_iter)[1];
 
-            if (x == max_width)
+            // // If the cell lies on the left, right, or bottom boundaries, differentiate it and add a cell label for the boundary conditions.
+            // if (x > (max_width - 0.75) )
+            // {
+            //     cell_iter->SetCellProliferativeType(p_diff_type);
+            //     cell_iter->AddCellProperty(p_label);
+            // }
+            // else if (x < (min_width + 0.75) )
+            // {
+            //     cell_iter->SetCellProliferativeType(p_diff_type);
+            //     cell_iter->AddCellProperty(p_label);
+            // }
+            // else 
+            if (y == min_height)
             {
+                cell_iter->SetCellProliferativeType(p_diff_type);
                 cell_iter->AddCellProperty(p_label);
             }
-            else if (x == min_width)
-            {
-                cell_iter->AddCellProperty(p_label);
-            }
-            else if (y == min_height)
-            {
-                cell_iter->AddCellProperty(p_label);
-            }
+
+            // // Turn the 'upper' part of the tissuo epidermis
+            // if (y > (max_height - 1.25))
+            // {
+            //     cell_iter->SetCellProliferativeType(p_stem_type);
+            // }
 
         }
 
@@ -160,15 +173,10 @@ public:
         simulator.SetEndTime(M_END_TIME); //Hopefully this is long enough for a steady state
 
         // Add linear spring force (modified to have three different spring stiffnesses, depending on the type of pair)
-        // MAKE_PTR(LinearSpringForceWithVariableRestLength<2>, p_spring_force);
-        // p_spring_force->SetCutOffLength(epithelial_epithelial_resting_spring_length);
-        // p_spring_force->SetEpithelialEpithelialSpringStiffness(epithelial_epithelial_stiffness); //Default is 15
-        // p_spring_force->SetEpithelialStromalSpringStiffness(epithelial_stromal_stiffness); //Default is 15
-        // p_spring_force->SetStromalStromalSpringStiffness(stromal_stromal_stiffness); //Default is 15
-        // p_spring_force->SetEpithelialEpithelialRestingSpringLength(epithelial_epithelial_resting_spring_length);
-        // p_spring_force->SetCutOffLength(radius_of_interaction);
-        // p_spring_force->SetMeinekeDivisionRestingSpringLength(division_separation);
-        // simulator.AddForce(p_spring_force);
+        // MAKE_PTR(GeneralisedLinearSpringForce<2>, p_spring_force);
+        MAKE_PTR(RepulsionForce<2>, p_spring_force);
+        p_spring_force->SetMeinekeSpringStiffness(30.0);
+        simulator.AddForce(p_spring_force);
 
         // Define the bottom, left, and right fixed boundaries.
         c_vector<double, 2> point, normal;
@@ -183,7 +191,7 @@ public:
         simulator.AddCellPopulationBoundaryCondition(p_bc_bottom);
 
         // // Left boundary
-        // point(0) = min_width + 0.25;
+        // point(0) = min_width + 0.75;
         // point(1) = 0.0;
         // normal(0) = -1.0;
         // normal(1) = 0.0;
@@ -192,7 +200,7 @@ public:
         // simulator.AddCellPopulationBoundaryCondition(p_bc_left);
 
         // // Right boundary
-        // point(0) = max_width - 0.25;
+        // point(0) = max_width - 0.75;
         // point(1) =  0.0;
         // normal(0) = 1.0;
         // normal(1) = 0.0;
